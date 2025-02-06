9a3d10063ad2920b392dcdba7e9fcc2c00dd35bd
jqtmviyu <jqtmviyu@gmail.com>
2025-02-06 20:30:59 +0800

    feat: :sparkles: 忽略多层文件夹和非视频字幕文件,支持多种语言字幕重命名

--- main.go
+++ main.go
@@ -9,18 +9,22 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"slices"
 	"strings"
 
 	"github.com/hekmon/transmissionrpc/v3"
 )
 
 type Config struct {
-	Username        string `json:"username"`
-	Password        string `json:"password"`
-	Host            string `json:"host"`
-	Port            int    `json:"port"`
-	SeasonPathRegex string `json:"seasonPathRegex"`
-	EpisodeRegex    string `json:"episodeRegex"`
+	Username        string   `json:"username"`
+	Password        string   `json:"password"`
+	Host            string   `json:"host"`
+	Port            int      `json:"port"`
+	SeasonPathRegex string   `json:"seasonPathRegex"`
+	EpisodeRegex    string   `json:"episodeRegex"`
+	Ext             []string `json:"ext"`
+	ExtSubs         []string `json:"extSubs"`
+	LangRegex       string   `json:"langRegex"`
 }
 
 func main() {
@@ -38,7 +42,7 @@ func main() {
 	// 尝试编译正则表达式
 	seasonPathRegex := regexp.MustCompile(config.SeasonPathRegex)
 	episodeRegex := regexp.MustCompile(config.EpisodeRegex)
-
+	langRegex := regexp.MustCompile(config.LangRegex)
 	// 检查正则表达式是否有效
 	if seasonPathRegex == nil {
 		log.Fatal("季度路径正则表达式无效")
@@ -46,6 +50,10 @@ func main() {
 
 	if episodeRegex == nil {
 		log.Fatal("集数正则表达式无效")
+	}
+
+	if langRegex == nil {
+		log.Fatal("多国语言正则表达式无效")
 	}
 
 	// 构建 Transmission RPC URL
@@ -116,15 +124,35 @@ func main() {
 				// log.Printf("跳过未选择下载的文件: %s", file.Name)
 				continue
 			}
-			oldPath := file.Name
-			oldBaseName := filepath.Base(oldPath)
+
+			oldPath := file.Name                             // 文件相对下载目录的路径
+			oldPath = strings.ReplaceAll(oldPath, "\\", "/") // 确保使用正斜杠
+			oldBaseName := filepath.Base(oldPath)            // 文件名
+			ext := filepath.Ext(oldPath)                     // 文件扩展名
 			log.Printf("处理文件: %s", oldPath)
+
+			// 如果文件路径包含的/超过两个,则跳过
+			if strings.Count(oldPath, "/") > 2 {
+				log.Printf("跳过多层文件夹: %s", oldPath)
+				continue
+			}
 
 			// 为每个文件提取季度信息
 			seasonNum := "01" // 默认为第一季
 			if seasonMatch := seasonPathRegex.FindStringSubmatch(oldPath); seasonMatch != nil {
 				seasonNum = fmt.Sprintf("%02s", seasonMatch[1])
 				// log.Printf("从文件路径中提取到季度: %s", seasonNum)
+			} else {
+				if strings.Count(oldPath, "/") > 1 {
+					log.Printf("未找到季度,跳过文件夹: %s", oldPath)
+					continue
+				}
+			}
+
+			// 如果文件不是视频文件或者字幕文件, 则跳过
+			if !slices.Contains(config.Ext, ext) && !slices.Contains(config.ExtSubs, ext) {
+				log.Printf("跳过非视频或字幕文件: %s", oldPath)
+				continue
 			}
 
 			// 检查文件名是否包含 SxxExx 格式
@@ -150,19 +178,22 @@ func main() {
 
 			// log.Printf("从文件名 %s 中提取到集数: %s", oldBaseName, episodeNum)
 
-			// 获取文件扩展名
-			ext := filepath.Ext(oldPath)
-
 			// 构建新文件名时保持相同的目录结构
-			newBaseName := fmt.Sprintf("%s S%sE%s%s",
+			// 如果文件是字幕文件, 则需要保留多语言后缀
+			lang := ""
+			if slices.Contains(config.ExtSubs, ext) {
+				oldBaseNameWithoutExt := oldBaseName[:len(oldBaseName)-len(ext)] // 去掉扩展名
+				if langMatch := langRegex.FindStringSubmatch(oldBaseNameWithoutExt); langMatch != nil {
+					lang = langMatch[0]
+				}
+			}
+			newBaseName := fmt.Sprintf("%s S%sE%s%s%s",
 				*torrent.Name,
 				seasonNum,
 				fmt.Sprintf("%02s", episodeNum),
+				lang,
 				ext,
 			)
-
-			// 确保使用正斜杠
-			oldPath = strings.ReplaceAll(oldPath, "\\", "/")
 
 			// 检查文件名是否相同
 			if oldBaseName == newBaseName {
